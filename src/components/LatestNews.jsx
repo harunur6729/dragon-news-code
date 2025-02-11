b6e7ad54944e38e2c1d1fb4ea08dff79f0e2743d
Harunur Rashid <142937596+harunur6729@users.noreply.github.com>
2025-02-11 12:54:08 +0600

    latest news added & marquee

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,19 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
+const LatestNews = () => {
+    return (
+        <div className="flex items-center bg-slate-300 py-2">
+            <p className="bg-[#D72050] text-white px-3 text-center font-bold py-1 text-2xl">Latest</p>
+            <Marquee pauseOnHover={true} speed={100}>
+                <Link to="/news">Lorem ipsum dolor sit amet consectetur adipisicing elit.</Link>
+                <Link to="/news">Lorem ipsum dolor sit amet consectetur adipisicing elit.</Link>
+                <Link to="/news">Lorem ipsum dolor sit amet consectetur adipisicing elit.</Link>
+                <Link to="/news">Lorem ipsum dolor sit amet consectetur adipisicing elit.</Link>
+                <Link to="/news">Lorem ipsum dolor sit amet consectetur adipisicing elit.</Link>
+            </Marquee>
+        </div>
+    );
+};
+
+export default LatestNews;
